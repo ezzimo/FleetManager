c2d28c8c84ab17ef19afebc6ba1c1596d6c2857f
ezzimo <ezzi.mohammed1@yahoo.fr>
2021-09-28 07:30:34 +0100

    nothing

--- account/views.py
+++ account/views.py
@@ -66,9 +66,7 @@ def account_register(request):
                 },
             )
             user.email_user(subject=subject, message=message)
-            return render(
-                request, "account/registration/register_email_confirm.html", context_instance=RequestContext(request)
-            )
+            return HttpResponseRedirect(request, "account/registration/register_email_confirm.html")
     else:
         registerForm = RegistrationForm
         return render(request, "account/registration/register.html", {"form": registerForm})
